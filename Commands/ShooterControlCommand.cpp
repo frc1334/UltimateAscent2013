5b28300178be0f95464089a42ed9d987081ea9bd
jamie0641 <jamie0641@hotmail.com>
2013-02-04 19:31:30 -0500

    Added tilt for bumpers

--- Commands/ShooterControlCommand.cpp
+++ Commands/ShooterControlCommand.cpp
@@ -20,6 +20,15 @@ void ShooterControlCommand::Execute()
 	 
 	 TriggerPressed = TriggerPressedPre;
 	shootersubsystem->SetFire(oi->ButtonGet());
+	if (GetButton = 5)
+			tilt += 10;
+		if (GetButton =6)
+			tilt-= 10;
+		if (tilt > ShooterSubsystem::maxDegrees)
+			tilt=maxDegrees;
+		if (tilt < ShooterSubsystem::minDegrees)
+			tilt=minDegrees;
+		shootersubsystem.SetTilt(tilt);
 }
  
 bool ShooterControlCommand::IsFinished()
