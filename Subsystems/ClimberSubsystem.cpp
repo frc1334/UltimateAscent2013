1434eacac86d944530344943afc5cf52acc705a8
josephd <joe@w00t42.net>
2013-02-07 20:12:25 -0500

    Revert "everything"
    This reverts commit 91a3bdb889d411355cda8f311640c3950a071549.

--- Subsystems/ClimberSubsystem.cpp
+++ Subsystems/ClimberSubsystem.cpp
@@ -1,60 +1,10 @@
 #include "ClimberSubsystem.h"
 #include "../Robotmap.h"
 
-ClimberSubsystem::ClimberSubsystem() : Subsystem("ClimberSubsystem"),
-leftMotor(CLIMB_MOTOR_LEFT),
-rightMotor(CLIMB_MOTOR_RIGHT),
-tiltMotor(CLIMB_MOTOR_TILT),
-leftEncoder(CLIMB_ENCODER_LEFT_A, CLIMB_ENCODER_LEFT_B),
-rightEncoder(CLIMB_ENCODER_RIGHT_A, CLIMB_ENCODER_RIGHT_B),
-tiltEncoder(CLIMB_ENCODER_TILT_A, CLIMB_ENCODER_TILT_B),
-tiltSwitch(CLIMB_LIMITSWITCH_TILT),
-leftSwitch(CLIMB_LIMITSWITCH_LEFT),
-rightSwitch(CLIMB_LIMITSWITCH_RIGHT),
-leftSolenoid(2,3),
-rightSolenoid(1),
-leftLoop(P,I,D,&leftEncoder,&leftMotor),
-rightLoop(P,I,D,&rightEncoder,&rightMotor),
-tiltLoop(P,I,D,&tiltEncoder, &tiltMotor)
+ClimberSubsystem::ClimberSubsystem() : Subsystem("ClimberSubsystem")
 {
-	leftEncoder.Start();
-	rightEncoder.Start();
-	tiltEncoder.Start();
-	leftEncoder.SetPIDSourceParameter(Encoder::kDistance);
-	rightEncoder.SetPIDSourceParameter(Encoder::kDistance);
-	tiltEncoder.SetPIDSourceParameter(Encoder::kDistance);
-	
 }
     
 void ClimberSubsystem::InitDefaultCommand()
 {
-}
-void ClimberSubsystem::SetPosition(int bar, bool tilt, float deg)
-{
-	if(bar == 0)
-	{
-		leftLoop.SetSetpoint(bar + 1);
-		rightLoop.SetSetpoint(bar + 1);
-	}
-	if(bar == 0 && tilt)
-	{
-		leftLoop.SetSetpoint(bar+1);
-		rightLoop.SetSetpoint(bar+1);
-		
-	}
-}
-int ClimberSubsystem::GetPosition()
-{
-	return currentBar;
-}
-bool ClimberSubsystem::IsAtTop()
-{
-	if(currentBar == 3)
-	{
-		return true;
-	}
-	else
-	{
-		return false;
-	}
 }
--- Subsystems/ClimberSubsystem.h
+++ Subsystems/ClimberSubsystem.h
@@ -7,22 +7,8 @@
 class ClimberSubsystem: public Subsystem
 {
 private:
-	static const int maxBar = 3;
-	static const int minBar = 0;//ground
-	static const int currentBar = 0; // Dummy value
-	static const float P = 0.0f;
-	static const float I = 0.0f;
-	static const float D = 0.0f;
-	Talon leftMotor, rightMotor, tiltMotor;
-	Encoder leftEncoder, rightEncoder, tiltEncoder;
-	PIDController leftLoop, rightLoop, tiltLoop;
-	Solenoid leftSolenoid, rightSolenoid;
-	DigitalInput tiltSwitch, leftSwitch, rightSwitch;
 public:
   ClimberSubsystem();
-  void SetPosition(int bar, bool tilt, float deg);
-  int GetPosition();
-  bool IsAtTop();
   void InitDefaultCommand();
 };
 
