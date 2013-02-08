45b930cf511311b1d4f4903e19583e18b19d74ac
josephd <joe@w00t42.net>
2013-02-07 20:46:46 -0500

    started auto mode

--- Commands/ShooterControlCommand.cpp
+++ Commands/ShooterControlCommand.cpp
@@ -3,12 +3,11 @@
 ShooterControlCommand::ShooterControlCommand()
 {
 	Requires(shootersubsystem);
-	TriggerPressedPre = false;
-	TriggerPressed = false;
-	LeftBumperPre = false;
-	RightBumperPre = false;
-	FirePre = false;
+	TriggerPressedPre = TriggerPressed = LeftBumperPre = RightBumperPre = FirePre = Manual = false;
 	tilt = 0;
+	setPoint = 0;
+	setPoints[0].speed = 0.0f;
+	setPoints[0].tilt  = 0.0f;
 }
 
 void ShooterControlCommand::Initialize()
@@ -25,14 +24,25 @@ void ShooterControlCommand::Execute()
 	TriggerPressedPre = TriggerPressed;
 	shootersubsystem->SetFire(oi->GetFire() && !FirePre);
 	FirePre = oi->GetFire();
-	if (oi->GetLeftBumper())
-		tilt += 0.1f;
-	if (oi->GetRightBumper())
-		tilt -= 0.1f;
-	if (tilt > ShooterSubsystem::maxDegrees)
-		tilt = ShooterSubsystem::maxDegrees;
-	if (tilt < ShooterSubsystem::minDegrees)
-		tilt = ShooterSubsystem::minDegrees;
+	if (Manual)
+	{
+		if (oi->GetLeftBumper())
+			tilt += 0.1f;
+		if (oi->GetRightBumper())
+			tilt -= 0.1f;
+		if (tilt > ShooterSubsystem::maxDegrees)
+			tilt = ShooterSubsystem::maxDegrees;
+		if (tilt < ShooterSubsystem::minDegrees)
+			tilt = ShooterSubsystem::minDegrees;
+	}
+	else
+	{
+		if (oi->GetLeftBumper())
+			setPoint += 1;
+		if (oi->GetRightBumper())
+			setPoint -= 1;
+		setPoint %= SETPOINT_COUNT;
+	}
 	LeftBumperPre = oi->GetLeftBumper();
 	RightBumperPre = oi->GetRightBumper();
 	shootersubsystem->SetTilt(tilt);
--- Commands/ShooterControlCommand.h
+++ Commands/ShooterControlCommand.h
@@ -1,13 +1,23 @@
 #ifndef SHOOTER_CONTROL_COMMAND_H
 #define SHOOTER_CONTROL_COMMAND_H
- 
+
+#define SETPOINT_COUNT 1
 #include "../CommandBase.h"
  
 class ShooterControlCommand: public CommandBase
 {
 private:
-	bool TriggerPressed, TriggerPressedPre, LeftBumperPre, RightBumperPre, FirePre;
+	struct SetPoint
+	{
+		float tilt;
+		float speed;
+		
+		SetPoint() : tilt(0.0f), speed(0.0f) {}
+		SetPoint(float t, float s) : tilt(t), speed(s) {}
+	} setPoints[SETPOINT_COUNT];
+	bool TriggerPressed, TriggerPressedPre, LeftBumperPre, RightBumperPre, FirePre, Manual;
 	float tilt;
+	int setPoint;
 public:
 	ShooterControlCommand();
 	virtual void Initialize();
