a7a3c5e6a8741075db1cde577a81542ce82a45f6
josephd <joe@w00t42.net>
2013-02-07 19:46:10 -0500

    Moved commit to the correct branch (yet again!)

--- RobotMap.h
+++ RobotMap.h
@@ -49,5 +49,36 @@
 #define DPAD_DOWNLEFT 19
 #define DPAD_DOWNRIGHT 20
 
+// ALL OF THESE ARE ZERO BASED!
+
+#define SHOOT_MOTOR                   1
+#define TILT_MOTOR_LEFT               2
+#define TILT_MOTOR_RIGHT              3
+#define TILT_ENCODER_LEFT_A           1
+#define TILT_ENCODER_LEFT_B           2
+#define TILT_ENCODER_RIGHT_A          3
+#define TILT_ENCODER_RIGHT_B          4
+#define TILT_SWITCH_LEFT			  5
+#define TILT_SWITCH_RIGHT			  6
+#define SHOOT_ENCODER				  7
+#define SHOOT_SOLENOID				  8
+
+#define CLIMB_SOLENOID_LEFT			  1
+#define CLIMB_MOTOR_LEFT			  2
+#define CLIMB_MOTOR_RIGHT             3
+#define CLIMB_ENCODER_RIGHT_A         4
+#define CLIMB_ENCODER_RIGHT_B         5
+#define CLIMB_ENCODER_LEFT_A          6
+#define CLIMB_ENCODER_LEFT_B          7
+#define CLIMB_LIMITSWITCH_TILT        8
+#define CLIMB_MOTOR_TILT              9
+#define CLIMB_SOLENOID_RIGHT          10
+#define CLIMB_LIMITSWITCH_LEFT        11
+#define CLIMB_LIMITSWITCH_RIGHT       12
+#define CLIMB_ENCODER_TILT_A          13
+#define CLIMB_ENCODER_TILT_B          14
+
+#define JOYSTICK_SHOOT                 1
+
 
 #endif
--- Subsystems/ClimberSubsystem.h
+++ Subsystems/ClimberSubsystem.h
@@ -15,9 +15,22 @@ private:
     PIDController left;
     PIDController right;
 
-};
+	static const int maxBar = 3;
+	static const int minBar = 0;//ground
+	static const int currentBar = 0; // Dummy value
+	static const float P = 0.0f;
+	static const float I = 0.0f;
+	static const float D = 0.0f;
+	Talon leftMotor, rightMotor, tiltMotor;
+	Encoder leftEncoder, rightEncoder, tiltEncoder;
+	PIDController leftLoop, rightLoop, tiltLoop;
+	Solenoid leftSolenoid, rightSolenoid;
+	DigitalInput tiltSwitch, leftSwitch, rightSwitch;
 public:
   ClimberSubsystem();
+  void SetPosition(int bar, bool tilt, float deg);
+  int GetPosition();
+  bool IsAtTop();
   void InitDefaultCommand();
   double returnPIDInput();
   int usePIDOutput(double output);
