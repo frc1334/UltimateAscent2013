0c121206dd555c4d7558b2ec1dabaa82aa66ef0e
Jamie Sinn <jman.sinn@gmail.com>
2013-02-12 19:46:11 -0500

    Removed Shooter Defines

--- RobotMap.h
+++ RobotMap.h
@@ -10,19 +10,6 @@
 */
 
 // ALL OF THESE ARE ZERO BASED!
-
-#define SHOOT_MOTOR                   1
-#define TILT_MOTOR_LEFT               2
-#define TILT_MOTOR_RIGHT              3
-#define TILT_ENCODER_LEFT_A           1
-#define TILT_ENCODER_LEFT_B           2
-#define TILT_ENCODER_RIGHT_A          3
-#define TILT_ENCODER_RIGHT_B          4
-#define TILT_SWITCH_LEFT			  5
-#define TILT_SWITCH_RIGHT			  6
-#define SHOOT_ENCODER				  7
-#define SHOOT_SOLENOID				  8
-
 #define CLIMB_MOTOR 				  1
 #define CLIMB_ENCODER_CLIMB_A         2
 #define CLIMB_ENCODER_CLIMB_B         3
