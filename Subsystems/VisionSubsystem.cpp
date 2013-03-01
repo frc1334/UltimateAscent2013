a4cf681c88e05a18bc8b9ee8762ea5d42b46d13f
josephd <joe@w00t42.net>
2013-02-28 20:21:41 -0500

    stuff

--- Subsystems/VisionSubsystem.cpp
+++ Subsystems/VisionSubsystem.cpp
@@ -16,19 +16,26 @@ void VisionSubsystem::InitDefaultCommand()
 
 void VisionSubsystem::UpdateTracking()
 {
-	if (camera.IsFreshImage())
+	try
 	{
-		ColorImage* img = NULL;
-		BinaryImage *b_img = NULL;
-		camera.GetImage(img);
-		// hue_high, hue_low, sat_high, sat_low, val_high, val_low
-		b_img = img->ThresholdHSV(85, 70, 255, 0, 240, 180);
-		//b_img = b_img->RemoveSmallObjects(true, 0);
-		//b_img = b_img->ConvexHull(true);
-		delete img;
-		delete b_img;
-		//vector<ParticleAnalysisReport>* particles = b_img->GetOrderedParticleAnalysisReports();
-		//for (unsigned int i = 0; i < particles->size(); i++)
-		//	std::cout << particles->at(i).center_mass_x << ", " << particles->at(i).center_mass_y  << ", " << (particles->at(i).boundingRect.width / particles->at(i).boundingRect.height) << std::endl;
+		if (camera.IsFreshImage())
+		{
+			ColorImage* img = camera.GetImage();
+			BinaryImage *b_img_1 = img->ThresholdHSV(70, 85, 0, 255, 180, 240);
+			delete img;
+			BinaryImage *b_img_2 = b_img_1->RemoveSmallObjects(true, 0);
+			delete b_img_1;
+			BinaryImage *b_img_3 = b_img_2->ConvexHull(true);
+			delete b_img_2;
+			vector<ParticleAnalysisReport>* particles = b_img_3->GetOrderedParticleAnalysisReports();
+			delete b_img_3;
+			cout << particles->size() << endl;
+			for (unsigned int i = 0; i < particles->size(); i++)
+				std::cout << particles->at(i).center_mass_x << ", " << particles->at(i).center_mass_y  << ", " << (particles->at(i).boundingRect.width / particles->at(i).boundingRect.height) << std::endl;
+		}
+	}
+	catch(exception& e)
+	{
+		cout << e.what() << endl;
 	}
 }
