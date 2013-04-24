745ad4d96f4e6239fb5de6643ef02f69667c5080
josephd <joe@w00t42.net>
2013-04-24 02:21:26 -0400

    made autonomous less configurable
    i write this on the bus; cant type

--- Commands/AutonomousCommandGroup.cpp
+++ Commands/AutonomousCommandGroup.cpp
@@ -8,23 +8,25 @@
 AutonomousCommandGroup::AutonomousCommandGroup()
 {
 	DigitalInput dg1(AUTONOMOUS_MODE1), dg2(AUTONOMOUS_MODE2), dg3(AUTONOMOUS_MODE3), dg4(AUTONOMOUS_MODE4);
-#ifdef DEBUG_CONSOLE
-	cout << "Autonomous Startup : " << (!dg1.Get() + (!dg2.Get() << 1)) << ", " << (!dg3.Get() + (!dg4.Get() << 1)) << endl;
-#endif
-	switch (!dg1.Get() + (!dg2.Get() << 1))
+	AddParallel(new AutonomousClimbCommand(1.15f, -0.5f));
+	AddSequential(new AutonomousShootCommand(4));
+	if (dg1.Get())
 	{
-	case 0:
-		AddParallel(new AutonomousClimbCommand(1.15f, -0.5f));
-		AddSequential(new AutonomousWaitCommand(4.0f));
-		if ((!dg3.Get() + (!dg4.Get() << 1)) == 0) break;
-	case 1:
-		AddSequential(new AutonomousShootCommand(4));
-		if ((!dg3.Get() + (!dg4.Get() << 1)) == 1) break;
-	case 2:
+		// do nothing, but maybe later we will do something here
+	}
+	else if (dg2.Get()) // straight back
+	{
 		AddSequential(new AutonomousDriveCommand(0.75f, 1.0f, 0.0f));
-		if ((!dg3.Get() + (!dg4.Get() << 1)) == 2) break;
-	case 3:
 		AddSequential(new AutonomousDriveCommand(0.55f, 0.0f, 1.0f));
-		if ((!dg3.Get() + (!dg4.Get() << 1)) == 3) break;
+	}
+	else if (dg3.Get()) // left
+	{
+		AddSequential(new AutonomousDriveCommand(0.75f, 1.0f, 0.0f));
+		AddSequential(new AutonomousDriveCommand(0.55f, 0.0f, 1.0f));
+	}
+	else if (dg4.Get()) // right
+	{
+		AddSequential(new AutonomousDriveCommand(0.75f, 1.0f, 0.0f));
+		AddSequential(new AutonomousDriveCommand(0.55f, 0.0f, 1.0f));
 	}
 }
