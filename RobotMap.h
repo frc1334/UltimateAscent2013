e5ae90236bd4f960d3de6bdcb0527668cfcf97c6
josephd <joe@w00t42.net>
2014-02-06 19:33:52 -0500

    cleaned up robotmap

--- RobotMap.h
+++ RobotMap.h
@@ -14,18 +14,18 @@
 #define CLIMB_MOTOR_1				7
 #define CLIMB_MOTOR_2				8
 #define CLIMB_SOLENOID				1
-#define CLIMB_RESET                 5
+#define CLIMB_RESET				5
 #define LEFT_MOTOR_1				2
 #define LEFT_MOTOR_2				3
 #define RIGHT_MOTOR_1				9
 #define RIGHT_MOTOR_2				10
-#define SHOOT_MOTOR					5
+#define SHOOT_MOTOR				5
 #define SHOOT_ENCODER				3
 #define SHOOT_SOLENOID				3
-#define SHOOT_TILT_SOLENOID         4
-#define TILT_MOTOR					4
+#define SHOOT_TILT_SOLENOID			4
+#define TILT_MOTOR				4
 #define DRIVE_SOLENOID				2
-#define TILT_LIMIT_SWITCH_TOP		2
+#define TILT_LIMIT_SWITCH_TOP			2
 #define COMPRESSOR_SWITCH			1
 #define COMPRESSOR_RELAY			1
 
